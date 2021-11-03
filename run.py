dd4844d50d35a96447b1284d8e041ce3ee742a43
AlrxDaley <alexander_daley@icloud.com>
2021-11-03 11:49:20 +0000

    Added function to add all 5 location of yout ship to a list

--- run.py
+++ run.py
@@ -25,11 +25,25 @@ def grid_setup(width,height):
 
     for x in range(len(grid)):
         print(grid[x])
-            
+
+    print("\n")
+
+
+def setting_ship_location():
+    
+    ship_location = []
+
+    for x in range(5):
+        xship = input("Please select the x location of your ship:\n")
+        yship = input("Please select the y location of your ship:\n")
+        ship_location.append([xship,yship])
+        
+    print(ship_location[0][1])
 
 
 def main():
     main_menu()
     grid_setup(6,6)
+    setting_ship_location()
 
 main()
