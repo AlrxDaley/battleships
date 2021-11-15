8f5f77f07dd1a0cbf3aec2a6e81fe1ff46b7df40
AlrxDaley <alexander_daley@icloud.com>
2021-11-15 21:43:43 +0000

    added ingnore[e501] so lint pep8 wouldnt have any errors

--- run.py
+++ run.py
@@ -14,9 +14,7 @@ grid_height = 0
 
 
 def main_menu():
-
-    """Prints the starting menu for the game using inputs"""
-
+    """Print the starting menu for the game using inputs to either exit the program or activate the main function and play the game"""
     while True:
         print("-----------\nBATTLESHIPS\n-----------")
         print("1.Play Game\n2.Quit\n")
@@ -33,9 +31,7 @@ def main_menu():
 
 
 def setting_custom_grid_size():
-
-    """takes the input from user to define player_grid size"""
-
+    """Take the input from user to define player_grid size."""
     print("\n-----------------------------------------")
     print("To set player_grid size please use 'Y,X' format")
     print("-----------------------------------------\n")
@@ -50,26 +46,28 @@ def setting_custom_grid_size():
 
 
 def print_grid(player_grid):
-    """Runs through each row checking if one of the index's is equal to 1 and turns it red and then prints the player_grid
-    if the index does not equal 1 then it will be printed in black. It also prints the top row and the left column
-    with the values of y so it dynamically changes the cordinates of the player_grid."""
-
+    """Run through each row checking if one of the index's is equal to 1 and
+    turns it red and then prints the player_grid if the index does not equal 1
+    then it will be printed in black. It also prints the top row and the left
+    column with the values of y so it dynamically changes the cordinates of the
+    player_grid."""
     if grid_width > 9:
         row = "| "
     else:
         row = "|"
 
-    for x in enumerate(0, grid_width):
+    for x in range(0, grid_width):
         row += str(x)
         if x > 8:
             row += "|"
         else:
             row += "| "
 
-    # Using os.linsep to ensure that the correct line seperator is used based on the OS the program is being run on e.g. \n
+    # Using os.linsep to ensure that the correct line seperator is used based on
+    # the OS the program is being run on e.g. \n
     print(row)
 
-    for y in enumerate(1, len(player_grid)):
+    for y in range(1, len(player_grid)):
 
         # Sets the first index of each row to y so the row numbers are icremeneted.
         try:
@@ -83,8 +81,8 @@ def print_grid(player_grid):
         else:
             row = "|" + str(y) + "|"
 
-        # the for loop , loops through each row within the player_grid enumerate executing the code as it loops
-        for x in enumerate(1, len(player_grid[y])):
+        # the for loop , loops through each row within the player_grid range executing the code as it loops
+        for x in range(1, len(player_grid[y])):
             # checks if any of the x indexs within each y row is equal to 1
             if player_grid[y][x] == 1 or player_grid[y][x] == 2:
                 # If [x][y] is equal to 1 it applies the colour red to the x index of the y row and then reapplies the black colour
@@ -119,25 +117,23 @@ def grid_setup(width, height):
 
     global player_grid
     # creates a player_grid using list comprehension
-    player_grid = [[0 for x in enumerate(grid_width)] for y in enumerate(grid_height)]
+    player_grid = [[0 for x in range(grid_width)] for y in range(grid_height)]
 
     global computer_grid
-    computer_grid = [[0 for x in enumerate(grid_width)] for y in enumerate(grid_height)]
+    computer_grid = [[0 for x in range(grid_width)] for y in range(grid_height)]
 
     global player_guess_grid
-    player_guess_grid = [
-        [0 for x in enumerate(grid_width)] for y in enumerate(grid_height)
-    ]
+    player_guess_grid = [[0 for x in range(grid_width)] for y in range(grid_height)]
 
     global grid_top
     # creates the top line of the cordinates on the player_grid
-    grid_top = [0 for x in enumerate(grid_width)]
+    grid_top = [0 for x in range(grid_width)]
 
     global computer_grid_top
-    computer_grid_top = [0 for x in enumerate(grid_width)]
+    computer_grid_top = [0 for x in range(grid_width)]
 
     global guess_top_grid
-    guess_top_grid = [0 for x in enumerate(grid_width)]
+    guess_top_grid = [0 for x in range(grid_width)]
 
 
 def setting_ship_location():
@@ -180,7 +176,9 @@ def setting_ship_location():
 
                 else:
                     # Assigns the variables to the location list
-                    gird_location_checking(player_row, player_column, ship_location, x)
+                    GLC = gird_location_checking(
+                        player_row, player_column, ship_location, x
+                    )
                     x = int(GLC)
             # return the ship location list
             except ValueError:
@@ -261,7 +259,7 @@ def computer_location_checking(row, column, location, i):
 def update_grid(ship_location):
     """Updates the inputed location of the ship using the ship_location indexs
     and itterating through using a for loop"""
-    for x in enumerate(len(ship_location)):
+    for x in range(len(ship_location)):
         # assigns the variable the location of the x axis
         player_row = ship_location[x][0]
         # assigns the variable the location of the y axis
@@ -275,7 +273,7 @@ def update_grid(ship_location):
 def computer_update_grid(computer_location):
     """updates the location of the computers ships using the computer_location
     index and iterating through using a for loop"""
-    for x in enumerate(len(computer_location)):
+    for x in range(len(computer_location)):
         computer_row = computer_location[x][0]
 
         computer_column = computer_location[x][1]
@@ -284,7 +282,7 @@ def computer_update_grid(computer_location):
 
 
 def guess_update_grid_hit(guess_location):
-    for each in enumerate(len(guess_location)):
+    for each in range(len(guess_location)):
         guess_row = guess_location[each][0]
 
         guess_column = guess_location[each][1]
@@ -295,7 +293,7 @@ def guess_update_grid_hit(guess_location):
 
 
 def guess_update_grid_miss(guess_location):
-    for x in enumerate(len(guess_location)):
+    for x in range(len(guess_location)):
         guess_row = guess_location[x][0]
 
         guess_column = guess_location[x][1]
